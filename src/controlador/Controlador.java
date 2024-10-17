35ffde7ba02157e3d154c2e1d0d1e646eef5c026
ramonmonmorenocesur <ramon.s384505@cesurformacion.com>
2024-10-17 12:40:10 +0200

    bolas

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -5,27 +5,128 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import modelo.Movimiento;
 
 public class Controlador implements Initializable {
     // atributos
+    private Circle[] bolas;
     @FXML
-    Circle circulo;
+    VBox escenarioPrincipal;
     @FXML
-    Pane principal;
-
+    Pane escenarioArriba;
+    @FXML
+    Pane escenarioAbajo;
+    @FXML
+    Label etiqueta1;
+    @FXML
+    Label etiqueta2;
+    @FXML
+    Label etiqueta3;
+    @FXML
+    Label etiqueta4;
+    @FXML
+    Label etiqueta5;
+    @FXML
+    Button botonPosicion;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        circulo.setLayoutX(principal.getPrefWidth() / 2);
-        circulo.setLayoutY(principal.getPrefHeight() / 2);
-        circulo.setRadius(5);
-        circulo.setFill(Color.RED);
+        bolas=new Circle[5];
+        escenarioPrincipal.setStyle("-fx-background-color:darkgreen");
+        escenarioArriba.setStyle("-fx-background-color:grey");
+        escenarioAbajo.setStyle("-fx-background-color:white");
+    }
 
-        Movimiento movimiento = new Movimiento(circulo, principal, 30);
-        movimiento.start();
+    public void etiquetaSeleccionada(MouseEvent evento) {
+        Label etiqueta = (Label) evento.getSource();
+        if (etiqueta1.getTextFill() == Color.RED) {
+            etiqueta1.setTextFill(Color.BLACK);
+        }
+        if (etiqueta2.getTextFill() == Color.RED) {
+            etiqueta2.setTextFill(Color.BLACK);
+        }
+        if (etiqueta3.getTextFill() == Color.RED) {
+            etiqueta3.setTextFill(Color.BLACK);
+        }
+        if (etiqueta4.getTextFill() == Color.RED) {
+            etiqueta4.setTextFill(Color.BLACK);
+        }
+        if (etiqueta5.getTextFill() == Color.RED) {
+            etiqueta5.setTextFill(Color.BLACK);
+        }
+        etiqueta.setTextFill(Color.RED);
+    }
+
+    public void posicionRaton(MouseEvent evento) {
+        int posicionX= (int)evento.getX();
+        int posicionY=(int)evento.getY();
+        
+        if (etiqueta1.getTextFill() == Color.RED) {
+            etiqueta1.setText("Bola1 (" +
+                    posicionX+ "," +posicionY + ")");
+                  iniciarBola(posicionX, posicionY, 1);
+                    
+        } else if (etiqueta2.getTextFill() == Color.RED) {
+            etiqueta2.setText("Bola2 (" +
+                    posicionX+ "," +posicionY+ ")");
+                    iniciarBola(posicionX, posicionY, 2);
+        } else if (etiqueta3.getTextFill() == Color.RED) {
+            etiqueta3.setText("Bola3 (" +
+                   posicionX + "," + posicionY + ")");
+                   iniciarBola(posicionX, posicionY, 3);
+        } else if (etiqueta4.getTextFill() == Color.RED) {
+            etiqueta4.setText("Bola4 (" +
+                    posicionX+ "," + posicionY + ")");
+                    iniciarBola(posicionX, posicionY, 4);
+        } else if (etiqueta5.getTextFill() == Color.RED) {
+            etiqueta5.setText("Bola5 (" +
+                   posicionX+ "," + posicionY + ")");
+                   iniciarBola(posicionX, posicionY, 5);
+        }
+
+    }
+     public void iniciarBola(int posicionX,int posicionY,int numBolas){
+        bolas[numBolas-1] = new Circle(posicionX,posicionY,5);
+
+    } 
+    public void posicionarBola(){
+        if (bolas[0]!=null&&etiqueta1.getTextFill()==Color.RED){
+            etiqueta1.setTextFill(Color.BLUE);
+            bolas[0].setFill(Color.RED);
+            escenarioArriba.getChildren().add(bolas[0]);
+        }
+        if (bolas[1]!=null&&etiqueta2.getTextFill()==Color.RED){
+            etiqueta2.setTextFill(Color.BLUE);
+            bolas[1].setFill(Color.RED);
+            escenarioArriba.getChildren().add(bolas[1]);
+        }
+        if (bolas[2]!=null&&etiqueta3.getTextFill()==Color.RED){
+            etiqueta3.setTextFill(Color.BLUE);
+            bolas[2].setFill(Color.RED);
+            escenarioArriba.getChildren().add(bolas[2]);
+        }
+        if (bolas[3]!=null&&etiqueta4.getTextFill()==Color.RED){
+            etiqueta4.setTextFill(Color.BLUE);
+            bolas[3].setFill(Color.RED);
+            escenarioArriba.getChildren().add(bolas[3]);
+        }
+        if (bolas[4]!=null&&etiqueta5.getTextFill()==Color.RED){
+            etiqueta5.setTextFill(Color.BLUE);
+            bolas[4].setFill(Color.RED);
+            escenarioArriba.getChildren().add(bolas[4]);
+        }
+        if(etiqueta1.getTextFill()==Color.BLUE&&etiqueta2.getTextFill()==Color.BLUE&&etiqueta3.getTextFill()==Color.BLUE&&etiqueta4.getTextFill()==Color.BLUE&&etiqueta5.getTextFill()==Color.BLUE){
+            botonPosicion.setVisible(false);
+        }
+       
+
+
+
     }
 
 }
--- src/modelo/App.java
+++ src/modelo/App.java
@@ -4,12 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class App extends Application{
@@ -18,22 +14,10 @@ public class App extends Application{
     public void start(Stage stage) throws IOException {
         
         Parent root = FXMLLoader.load(getClass().getResource(".\\..\\vista\\vista.fxml"));
-        
-       
-        /* 
-        Group root = new Group();
-        Circle circulo = new Circle();
-        circulo.setLayoutX(300);
-        circulo.setLayoutY(200);
-        circulo.setRadius(5);
-        circulo.setFill(Color.RED);
+                
 
-        root.getChildren().add(circulo);
-        */
-        
-
-        stage.setTitle("movimiento");
-        stage.setScene(new Scene(root,600,400));
+        stage.setTitle("ProyectoNuevo");
+        stage.setScene(new Scene(root,600,600));
         stage.setResizable(false);
         stage.show();
     }
